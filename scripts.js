c41b71dc163cb2b0563baaba60ae7adf284c9f88
Pascal <77117642+Pascal273@users.noreply.github.com>
2021-11-10 18:09:04 +0100

    converted imageslider class to funtion

--- scripts.js
+++ scripts.js
@@ -1,68 +1,67 @@
-class Imageslider {
-  constructor(title, listOfImages) {
-    this.numberOfSections = Math.ceil(listOfImages.length / 4);
-    this.listOfImageChunks = sliceIntoChunks(listOfImages, 4);
+function createImageSlider(title, listOfImages) {
+  let numberOfSections = Math.ceil(listOfImages.length / 4);
+  let listOfImageChunks = sliceIntoChunks(listOfImages, 4);
 
-    this.newHeader = document.createElement("h1");
-    this.newHeader.className = "sectionHeader";
-    this.newHeader.innerText = title;
-    this.title = title;
+  let newHeader = document.createElement("h1");
+  newHeader.className = "sectionHeader";
+  newHeader.innerText = title;
 
-    this.divScrollbar = document.createElement("div");
-    this.divScrollbar.classList.add("categoryScrollbar");
+  let divScrollbar = document.createElement("div");
+  divScrollbar.classList.add("categoryScrollbar");
 
-    this.createSections();
+  let count = 1;
+  for (const chunk of listOfImageChunks) {
+    window["section" + count] = document.createElement("section");
+    let section = window["section" + count];
+    section.id = `section${count}${title}`;
 
-    document.getElementById("imageSlider").appendChild(this.newHeader);
-    document.getElementById("imageSlider").appendChild(this.divScrollbar);
+    let arrowLeft = document.createElement("a");
+    arrowLeft.href = `#section${lastSectionNumber(
+      count,
+      numberOfSections
+    )}${title}`;
+    arrowLeft.classList.add("arrowButton", "leftArrow");
+
+    section.appendChild(arrowLeft);
+
+    for (const thumb of chunk) {
+      let img = document.createElement("div");
+      img.classList.add("movieImages");
+      img.style.backgroundImage = `url(${thumb})`;
+
+      section.appendChild(img);
+    }
+
+    let arrowRight = document.createElement("a");
+    arrowRight.href = `#section${nextSectionNumber(
+      count,
+      numberOfSections
+    )}${title}`;
+    arrowRight.classList.add("arrowButton", "rightArrow");
+
+    section.appendChild(arrowRight);
+
+    divScrollbar.appendChild(section);
+    count += 1;
   }
 
-  createSections() {
-    let count = 1;
-    for (const chunk of this.listOfImageChunks) {
-      window["section" + count] = document.createElement("section");
-      let section = window["section" + count];
-      section.id = `section${count}${this.title}`;
+  document.getElementById("imageSlider").appendChild(newHeader);
+  document.getElementById("imageSlider").appendChild(divScrollbar);
+}
 
-      let arrowLeft = document.createElement("a");
-      arrowLeft.href = `#section${this.lastSectionNumber(count)}${this.title}`;
-      arrowLeft.classList.add("arrowButton", "leftArrow");
-
-      section.appendChild(arrowLeft);
-
-      for (const thumb of chunk) {
-        let img = document.createElement("img");
-        img.classList.add("movieImages");
-        img.src = thumb;
-
-        section.appendChild(img);
-      }
-
-      let arrowRight = document.createElement("a");
-      arrowRight.href = `#section${this.nextSectionNumber(count)}${this.title}`;
-      arrowRight.classList.add("arrowButton", "rightArrow");
-
-      section.appendChild(arrowRight);
-
-      this.divScrollbar.appendChild(section);
-      count += 1;
-    }
+function nextSectionNumber(sectionNumber, numberOfSections) {
+  if (sectionNumber == numberOfSections) {
+    return 1;
+  } else {
+    return sectionNumber + 1;
   }
+}
 
-  nextSectionNumber(sectionNumber) {
-    if (sectionNumber == this.numberOfSections) {
-      return 1;
-    } else {
-      return sectionNumber + 1;
-    }
-  }
-
-  lastSectionNumber(sectionNumber) {
-    if (sectionNumber == 1) {
-      return this.numberOfSections;
-    } else {
-      return sectionNumber - 1;
-    }
+function lastSectionNumber(sectionNumber, numberOfSections) {
+  if (sectionNumber == 1) {
+    return numberOfSections;
+  } else {
+    return sectionNumber - 1;
   }
 }
 
@@ -79,7 +78,7 @@ const imgList = Array(12).fill(
   "https://images.unsplash.com/photo-1626191587911-45c8729b8d99?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80"
 );
 
-let topRated = new Imageslider("Top Rated Movies", imgList);
-let category1 = new Imageslider("Action", imgList);
-let category2 = new Imageslider("Crime", imgList);
-let category3 = new Imageslider("Comedy", imgList);
+let topRated = createImageSlider("Top Rated Movies", imgList);
+let category1 = createImageSlider("Action", imgList);
+let category2 = createImageSlider("Crime", imgList);
+let category3 = createImageSlider("Comedy", imgList);
