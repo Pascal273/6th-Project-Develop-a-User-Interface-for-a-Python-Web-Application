60f3f743dc1456822c64a3d040c014ff0820011c
Pascal <77117642+Pascal273@users.noreply.github.com>
2021-11-13 22:35:50 +0100

    section indicator added

--- scripts.js
+++ scripts.js
@@ -12,22 +12,52 @@ window.addEventListener("scroll", function () {
   }
 });
 
+function disableWindowScroll() {
+  winX = window.scrollX;
+  winY = window.scrollY;
+}
+
 function createImageSlider(title, listOfImages) {
+  const titleNoSpaces = title.replaceAll(" ", "_");
+
   let numberOfSections = Math.ceil(listOfImages.length / imgPerSection);
   let listOfImageChunks = sliceIntoChunks(listOfImages, imgPerSection);
 
-  let newHeader = document.createElement("h1");
+  let newHeader = document.createElement("h2");
   newHeader.className = "sectionHeader";
   newHeader.innerText = title;
 
+  let sectionIndicator = document.createElement("ul");
+  sectionIndicator.className = "sectionIndicator";
+  sectionIndicator.id = titleNoSpaces;
+  sectionIndicator.style.visibility = "hidden";
+
+  for (let i = 0; i < numberOfSections; i++) {
+    let li = document.createElement("li");
+    li.className = "";
+    sectionIndicator.appendChild(li);
+  }
+  sectionIndicator.firstElementChild.className = "active";
+
+  let sliderHeadline = document.createElement("div");
+  sliderHeadline.className = "flexProperties";
+  sliderHeadline.appendChild(newHeader);
+  sliderHeadline.appendChild(sectionIndicator);
+
   let divScrollbar = document.createElement("div");
   divScrollbar.classList.add("categoryScrollbar");
+  divScrollbar.addEventListener("mouseover", function () {
+    document.getElementById(titleNoSpaces).style.visibility = "visible";
+  });
+  divScrollbar.addEventListener("mouseout", function () {
+    document.getElementById(titleNoSpaces).style.visibility = "hidden";
+  });
 
-  let count = 1;
+  let sectionNumber = 1;
   for (const chunk of listOfImageChunks) {
-    window["section" + count] = document.createElement("section");
-    let section = window["section" + count];
-    section.id = `section${count}${title}`;
+    window["section" + sectionNumber] = document.createElement("section");
+    let section = window["section" + sectionNumber];
+    section.id = `section${sectionNumber}${title}`;
 
     // if a section is incomplete the thumbnails will be aligned left to right
     // else they will have an even space in between
@@ -39,12 +69,22 @@ function createImageSlider(title, listOfImages) {
 
     let arrowLeft = document.createElement("a");
     arrowLeft.textContent = "‹";
-    arrowLeft.onmousedown = function disableWindowScroll() {
-      winX = window.scrollX;
-      winY = window.scrollY;
+    arrowLeft.onmousedown = function () {
+      disableWindowScroll();
+
+      const newContent = highlightLastSectionIndicator(
+        document.querySelectorAll(`#${titleNoSpaces} li`)
+      );
+
+      let ul = document.getElementById(`${titleNoSpaces}`);
+      ul.innerHTML = "";
+      for (const li of newContent) {
+        ul.appendChild(li);
+      }
     };
+
     arrowLeft.href = `#section${lastSectionNumber(
-      count,
+      sectionNumber,
       numberOfSections
     )}${title}`;
     arrowLeft.classList.add("arrowButton", "leftArrow");
@@ -61,7 +101,7 @@ function createImageSlider(title, listOfImages) {
       let img = document.createElement("img");
       img.src = thumb;
 
-      let heading = document.createElement("h1");
+      let heading = document.createElement("h3");
       heading.textContent = "Movie Title";
       heading.className = "heading";
 
@@ -80,13 +120,22 @@ function createImageSlider(title, listOfImages) {
 
     let arrowRight = document.createElement("a");
     arrowRight.textContent = "›";
-    arrowRight.onmousedown = function disableWindowScroll() {
-      winX = window.scrollX;
-      winY = window.scrollY;
+    arrowRight.onmousedown = function () {
+      disableWindowScroll();
+
+      const newContent = highlightNextSectionIndicator(
+        document.querySelectorAll(`#${titleNoSpaces} li`)
+      );
+
+      let ul = document.getElementById(`${titleNoSpaces}`);
+      ul.innerHTML = "";
+      for (const li of newContent) {
+        ul.appendChild(li);
+      }
     };
 
     arrowRight.href = `#section${nextSectionNumber(
-      count,
+      sectionNumber,
       numberOfSections
     )}${title}`;
     arrowRight.classList.add("arrowButton", "rightArrow");
@@ -94,10 +143,10 @@ function createImageSlider(title, listOfImages) {
     section.appendChild(arrowRight);
 
     divScrollbar.appendChild(section);
-    count += 1;
+    sectionNumber += 1;
   }
 
-  document.getElementById("imageSlider").appendChild(newHeader);
+  document.getElementById("imageSlider").appendChild(sliderHeadline);
   document.getElementById("imageSlider").appendChild(divScrollbar);
 }
 
@@ -124,6 +173,20 @@ function sliceIntoChunks(arr, chunkSize) {
     res.push(chunk);
   }
   return res;
+}
+
+function highlightNextSectionIndicator(nodeListElements) {
+  let listElements = Array.from(nodeListElements);
+  listElements.unshift(listElements.pop());
+
+  return listElements;
+}
+
+function highlightLastSectionIndicator(nodeListElements) {
+  let listElements = Array.from(nodeListElements);
+  listElements.push(listElements.shift());
+
+  return listElements;
 }
 
 const imgList = Array(12).fill(
